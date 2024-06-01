8e7e2dbfc5c8218a44525ff1d04abb07f591756f
畑原僚太 <hatahararyoufutoshi@hatahararyoufutoshinoMacBook-Air.local>
2024-06-01 23:17:48 +0900

    投稿一覧画面、新規投稿画面、投稿詳細画面 土台

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "投稿が成功しました。"
+      redirect_to posts_path
+    else
+      flash[:alert] = "投稿に失敗しました。"
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :title, :location, :start_date, :end_date)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_27_081926) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_28_121458) do
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "image"
+    t.string "location"
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", null: false
     t.string "crypted_password"
