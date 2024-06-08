36bdd361ca0f50018627362316c36df2b80b6ccf
畑原僚太 <hatahararyoufutoshi@hatahararyoufutoshinoMacBook-Air.local>
2024-06-08 15:58:04 +0900

    ログイン中のユーザーネームを表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,8 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.user = current_user
+
     if @post.save
       flash[:notice] = "投稿が成功しました。"
       redirect_to posts_path
